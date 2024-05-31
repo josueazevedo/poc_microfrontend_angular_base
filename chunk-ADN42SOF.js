5ced7d5961c9d443f03dffa8b9f4483c69492acb
josueazevedo <josueazevedo@users.noreply.github.com>
2024-05-31 00:41:52 +0000

    deploy: 30c2ac54c5b8b9d37b6b891303bad0deff8ab457

--- chunk-ADN42SOF.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-35PI25VP.js";var t=[{path:"",loadComponent:()=>import("./chunk-3GRMM3UO.js").then(o=>o.AboutComponent)}];export{t as homeRoutes};
---
+++ chunk-MJ2HAYVK.js
@@ -0,0 +1,1 @@
+import"./chunk-35PI25VP.js";var t=[{path:"",loadComponent:()=>import("./chunk-O6MU7ZRL.js").then(o=>o.AboutComponent)}];export{t as homeRoutes};
